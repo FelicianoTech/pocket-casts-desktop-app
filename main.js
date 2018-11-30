3d44daee7868f92ecf037ec8844bb7157e96d982
Ricardo N Feliciano <FelicianoTech@gmail.com>
2018-11-30 11:06:37 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,31 @@
+const electron = require( "electron" );
+const { shell, app, BrowserWindow } = electron;
+const HOMEPAGE = "https://playbeta.pocketcasts.com/web/"
+
+let mainWindow;
+
+app.on( "ready", () => {
+	window = new BrowserWindow({
+		width: 1200,
+		height: 900,
+		webPreferences: {
+			nodeIntegration: false
+		}
+	});
+
+	window.setMenuBarVisibility( false );
+	window.loadURL( HOMEPAGE );
+
+	window.webContents.on( "will-navigate", ( ev, url ) => {
+		parts = url.split( '/' );
+
+		if( parts[0] + "//" + parts[2] != HOMEPAGE ){
+			ev.preventDefault();
+			shell.openExternal( url );
+		}
+	});
+
+	window.on( "closed", () => {
+		window = null;
+	});
+});
